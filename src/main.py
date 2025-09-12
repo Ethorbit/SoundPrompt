29d9e74a1b937c87700d539c8386a3af9fdb6be1
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-09-12 01:57:39 -0700

    Add retrieval.py, finish file scoring

--- src/main.py
+++ src/main.py
@@ -18,9 +18,8 @@
 # GNU General Public License along with SoundPrompt.
 # If not, see <https://www.gnu.org/licenses/>.
 #
-
 from config import args, config as config_import
-from data import database
+from data import database, retrieval
 from sentence_transformers import SentenceTransformer
 cfg = config_import.load_config()
 
@@ -46,63 +45,30 @@ if args.load:
         try:
             # TODO: make interactive prompt have a history like a terminal
             prompt = args.prompt or input(">>>").lower()
-
             prompt_embedding = model.encode(prompt.lower())
-            top_n_results = collection.query(
-                query_embeddings=[prompt_embedding],
-                n_results=10
+
+            top_results = retrieval.get_top_results(
+                collection,
+                embedding=prompt_embedding,
+                limit=10
             )
 
-            # TODO: add Reranking & Rerank model
-            # The current query is pretty accurate, but
-            # there are a few times where out of the 10
-            # items, the first is not the most accurate
-            # Reranking can solve that issue
+            deduplicated_top_results = retrieval.deduplicate_results(
+                top_results
+            )
 
-            # Deduplicate, keep only the first tag of a file
-            unique_metadatas = []
-            unique_distances = []
-            seen_files = set()
-            for metadata, distance in zip(
-                top_n_results["metadatas"][0],
-                top_n_results["distances"][0]
-            ):
-                file_path = metadata["tag_file"]
-                if file_path not in seen_files:
-                    seen_files.add(file_path)
-                    unique_metadatas.append(metadata)
-                    unique_distances.append(distance)
+            scored_files = retrieval.get_cumulative_file_scores(
+                collection,
+                embedding=prompt_embedding,
+                result=deduplicated_top_results
+            )
 
-            distinct_top_n_results = {
-                "metadatas": [unique_metadatas],
-                "distances": [unique_distances]
-            }
+            selected_file = retrieval.get_highest_scored_file(scored_files)
 
-            # We need to do chunk-level scoring with file-level
-            # aggregation
-            #
-            # 1. Each tag is treated as a chunk
-            # 2. We compute the similarity between the query
-            # embedding and the embedding of each tag individually.
-            # 3. This gives a score per chunk.
+            print(scored_files)
+            print(selected_file)
 
-            for meta, dist in zip(
-                distinct_top_n_results["metadatas"][0],
-                distinct_top_n_results["distances"][0]
-            ):
-                # Get meta file_path, do query to get all tags OF file_path
-                # Create a list of scores by iterating each tag and appending
-                # the simularity from the tag to the prompt
-                #
-                # Choose the file with the best score - that's the match!
-                file_results = collection.get(
-                    where={"tag_file": meta["tag_file"]}
-                )
-
-                for file_result in file_results["metadatas"]:
-                    print(file_result["tag_file"])
-                    print(file_result["tag"])
-
+            # If it's not interactive, stop immediately
             if args.prompt:
                 break
         except KeyboardInterrupt:
